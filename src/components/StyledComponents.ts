614d073fd4e11ea88eb8fd7e7fa48a6bf12c6865
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-27 13:20:10 +0300

    style(components): add new styled components

--- src/components/StyledComponents.ts
+++ src/components/StyledComponents.ts
@@ -13,10 +13,18 @@ export const StyledButton = styled.button`
   }
 `
 export const StyledInputText = styled.input`
+  width: 300px;
   font-size: 1.5rem;
   padding: 5px;
   margin-bottom: 8px;
   &:focus {
     outline-color: cyan;
   }
+`
+export const StyledField = styled.label`
+  font-size: 2rem;
+  display: flex;
+  width: 90%;
+  justify-content: space-between;
+  margin-bottom: 10px;
 `
