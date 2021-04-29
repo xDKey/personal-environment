d373c97cade3375c601b9d2c75191f83720b8599
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-29 14:17:32 +0300

    test(userDataMock): separate user datamock definition

--- src/components/tests/DisplayUserInfo.spec.tsx
+++ src/components/tests/DisplayUserInfo.spec.tsx
@@ -1,40 +1,31 @@
 import { mount } from '@cypress/react'
 import DisplayUserInfo from '../DisplayUserInfo'
-import { HomePageUserInfoQueryResponse } from '../__generated__/HomePageUserInfoQuery.graphql'
+import { userResponseMock } from './constants'
 
-describe('<DisplayUserInfo />', () => {
-  const data: HomePageUserInfoQueryResponse = {
-    user: {
-      id: '1',
-      name: 'Test Name',
-      email: 'Test Email',
-      bio: null,
-      age: 20,
-    },
-  }
-  it('Render correctly', () => {
-    mount(<DisplayUserInfo data={data} />)
+it('Render correctly', () => {
+  mount(<DisplayUserInfo data={userResponseMock} />)
 
-    cy.get('label').should('have.length', 4)
+  const { user } = userResponseMock
 
-    cy.get('label')
-      .contains('Email')
-      .parent()
-      .should('have.text', `Email: ${data.user.email}`)
+  cy.get('label').should('have.length', 4)
 
-    cy.get('label')
-      .contains('Name')
-      .parent()
-      .should('have.text', `Name: ${data.user.name}`)
+  cy.get('label')
+    .contains('Email')
+    .parent()
+    .should('have.text', `Email: ${user.email}`)
 
-    cy.get('label')
-      .contains('Age')
-      .parent()
-      .should('have.text', `Age: ${data.user.age}`)
+  cy.get('label')
+    .contains('Name')
+    .parent()
+    .should('have.text', `Name: ${user.name}`)
 
-    cy.get('label')
-      .contains('Biography')
-      .parent()
-      .should('have.text', `Biography: unknown`)
-  })
+  cy.get('label')
+    .contains('Age')
+    .parent()
+    .should('have.text', `Age: ${user.age}`)
+
+  cy.get('label')
+    .contains('Biography')
+    .parent()
+    .should('have.text', `Biography: unknown`)
 })
--- src/components/tests/constants.ts
+++ src/components/tests/constants.ts
@@ -1,1 +1,13 @@
+import type { HomePageUserInfoQueryResponse } from '../__generated__/HomePageUserInfoQuery.graphql'
+
 export const SERVER_URL = 'http://localhost:4000/'
+
+export const userResponseMock: HomePageUserInfoQueryResponse = {
+  user: {
+    id: '1',
+    name: 'Test Name',
+    email: 'Test Email',
+    bio: null,
+    age: 20,
+  },
+}
