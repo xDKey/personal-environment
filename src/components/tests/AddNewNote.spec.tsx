45f3660d7fbacd4a6fb7ad550ad9d783f6a6e3db
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-29 14:14:11 +0300

    test: remove unnecessary describes

--- src/components/tests/AddNewNote.spec.tsx
+++ src/components/tests/AddNewNote.spec.tsx
@@ -4,44 +4,42 @@ import RelayEnvironment from '../../utils/RelayEnvironment'
 import AddNewNote from '../AddNewNote'
 import { SERVER_URL } from './constants'
 
-describe('<AddNewNote />', () => {
-  beforeEach(() => {
-    mount(
-      <RelayEnvironmentProvider environment={RelayEnvironment}>
-        <AddNewNote setShowAddNote={cy.stub().as('setShowAddNote')} />
-      </RelayEnvironmentProvider>
-    )
-    cy.intercept('POST', SERVER_URL, {
-      data: {
-        addNote: 'check',
-      },
-    }).as('request')
-  })
+beforeEach(() => {
+  mount(
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <AddNewNote setShowAddNote={cy.stub().as('setShowAddNote')} />
+    </RelayEnvironmentProvider>
+  )
+  cy.intercept('POST', SERVER_URL, {
+    data: {
+      addNote: 'check',
+    },
+  }).as('request')
+})
 
-  it('Render correctly', () => {
-    cy.get('button[type="submit"]').should('have.text', 'Save')
-    cy.get('input').should('have.length', 2)
-  })
+it('Render correctly', () => {
+  cy.get('button[type="submit"]').should('have.text', 'Save')
+  cy.get('input').should('have.length', 2)
+})
 
-  it('Should not send request if not title field filled in', () => {
-    cy.get('input').last().type('check{enter}')
-    cy.get('@setShowAddNote').should('not.have.been.called')
-  })
+it('Should not send request if not title field filled in', () => {
+  cy.get('input').last().type('check{enter}')
+  cy.get('@setShowAddNote').should('not.have.been.called')
+})
 
-  it('Should send request if title field filled in', () => {
-    cy.get('input').first().type('check{enter}')
-    cy.wait('@request').then((fetch) => {
-      const { request } = fetch
-      expect(request.body).to.include.all.keys('query', 'variables')
-      expect(request.body.variables).to.deep.equal({
-        title: 'check',
-        description: '',
-      })
+it('Should send request if title field filled in', () => {
+  cy.get('input').first().type('check{enter}')
+  cy.wait('@request').then((fetch) => {
+    const { request } = fetch
+    expect(request.body).to.include.all.keys('query', 'variables')
+    expect(request.body.variables).to.deep.equal({
+      title: 'check',
+      description: '',
     })
-
-    cy.get('input').last().type('description{enter}')
-    cy.wait('@request')
-      .its('request.body.variables')
-      .should('deep.equal', { title: 'check', description: 'description' })
   })
+
+  cy.get('input').last().type('description{enter}')
+  cy.wait('@request')
+    .its('request.body.variables')
+    .should('deep.equal', { title: 'check', description: 'description' })
 })
--- src/components/tests/AuthorizeForm.spec.tsx
+++ src/components/tests/AuthorizeForm.spec.tsx
@@ -5,65 +5,63 @@ import RelayEnvironment from '../../utils/RelayEnvironment'
 import AuthorizeForm from '../AuthorizeForm'
 import { SERVER_URL } from './constants'
 
-describe('<AuthorizeForm />', () => {
-  beforeEach(() => {
-    mount(
-      <RelayEnvironmentProvider environment={RelayEnvironment}>
-        <BrowserRouter>
-          <AuthorizeForm setIsLogged={cy.stub().as('setIsLogged')} />
-        </BrowserRouter>
-      </RelayEnvironmentProvider>
-    )
-    cy.intercept('POST', SERVER_URL).as('request')
+beforeEach(() => {
+  mount(
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <BrowserRouter>
+        <AuthorizeForm setIsLogged={cy.stub().as('setIsLogged')} />
+      </BrowserRouter>
+    </RelayEnvironmentProvider>
+  )
+  cy.intercept('POST', SERVER_URL).as('request')
+})
+
+it('Render correctly', () => {
+  cy.get('h1').should('have.text', 'Please log in or register')
+  cy.get('input').should('have.length', 2)
+  cy.get('button[type="submit"]').should('have.text', 'Authorize')
+
+  cy.contains('register').click()
+  cy.get('h1').should('have.text', 'Please register or log in')
+})
+
+it('Should not send request if not all fields filled in', () => {
+  cy.get('input[type="email"]').type('admin@admin{enter}')
+  cy.get('@setIsLogged').should('not.have.been.called')
+
+  cy.get('input[type="email"]').clear()
+  cy.get('input[type="password"]').type('admin{enter}')
+  cy.get('@setIsLogged').should('not.have.been.called')
+
+  cy.contains('register').click()
+
+  cy.get('input[type="password"]').clear()
+  cy.get('input[type="text"]').type('admin')
+  cy.get('input[type="email"]').type('admin@admin{enter}')
+  cy.get('@setIsLogged').should('not.have.been.called')
+
+  cy.get('input[type="text"]').clear()
+  cy.get('input[type="password"]').type('admin{enter}')
+  cy.get('@setIsLogged').should('not.have.been.called')
+})
+
+it('Should display an error message and not receive a token if the entered data is invalid', () => {
+  cy.get('input[type="email"]').type('admin@admin')
+  cy.get('input[type="password"]').type('a{enter}')
+
+  cy.contains('Such user not found or password is invalid')
+
+  cy.wait('@request').then(({ response }) => {
+    assert(response?.body?.data?.login === null)
   })
+})
 
-  it('Render correctly', () => {
-    cy.get('h1').should('have.text', 'Please log in or register')
-    cy.get('input').should('have.length', 2)
-    cy.get('button[type="submit"]').should('have.text', 'Authorize')
+it('Should receive a token if entered data is valid', () => {
+  cy.get('input[type="email"]').type('admin@admin')
+  cy.get('input[type="password"]').type('admin{enter}')
 
-    cy.contains('register').click()
-    cy.get('h1').should('have.text', 'Please register or log in')
-  })
-
-  it('Should not send request if not all fields filled in', () => {
-    cy.get('input[type="email"]').type('admin@admin{enter}')
-    cy.get('@setIsLogged').should('not.have.been.called')
-
-    cy.get('input[type="email"]').clear()
-    cy.get('input[type="password"]').type('admin{enter}')
-    cy.get('@setIsLogged').should('not.have.been.called')
-
-    cy.contains('register').click()
-
-    cy.get('input[type="password"]').clear()
-    cy.get('input[type="text"]').type('admin')
-    cy.get('input[type="email"]').type('admin@admin{enter}')
-    cy.get('@setIsLogged').should('not.have.been.called')
-
-    cy.get('input[type="text"]').clear()
-    cy.get('input[type="password"]').type('admin{enter}')
-    cy.get('@setIsLogged').should('not.have.been.called')
-  })
-
-  it('Should display an error message and not receive a token if the entered data is invalid', () => {
-    cy.get('input[type="email"]').type('admin@admin')
-    cy.get('input[type="password"]').type('a{enter}')
-
-    cy.contains('Such user not found or password is invalid')
-
-    cy.wait('@request').then(({ response }) => {
-      assert(response?.body?.data?.login === null)
-    })
-  })
-
-  it('Should receive a token if entered data is valid', () => {
-    cy.get('input[type="email"]').type('admin@admin')
-    cy.get('input[type="password"]').type('admin{enter}')
-
-    cy.get('@setIsLogged').should('have.been.called')
-    cy.wait('@request').then(({ response }) => {
-      assert(response?.body.data.login.token)
-    })
+  cy.get('@setIsLogged').should('have.been.called')
+  cy.wait('@request').then(({ response }) => {
+    assert(response?.body.data.login.token)
   })
 })
