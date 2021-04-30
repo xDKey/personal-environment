505230b69cf8a3f3b721d0322a670fcfa828e43d
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-30 14:16:08 +0300

    test(HomePage): add render's tests

--- src/components/tests/HomePage.spec.tsx
+++ src/components/tests/HomePage.spec.tsx
@@ -20,12 +20,36 @@ it('Display message "Please, log in" if there isnt token in localstorage', () =>
 })
 
 it('Render correctly if there is token in localstorage', () => {
-
   cy.intercept(SERVER_URL, {
     data: userResponseMock,
-  }).as('fetch')
+  })
 
   setUpComponent(true)
+
+  const { user } = userResponseMock
+
+  cy.get('label').should('have.length', 4)
+
+  cy.get('label')
+    .contains('Email')
+    .parent()
+    .should('have.text', `Email: ${user.email}`)
+
+  cy.get('label')
+    .contains('Name')
+    .parent()
+    .should('have.text', `Name: ${user.name}`)
+
+  cy.get('label')
+    .contains('Age')
+    .parent()
+    .should('have.text', `Age: ${user.age}`)
+
+  cy.get('label')
+    .contains('Biography')
+    .parent()
+    .should('have.text', `Biography: unknown`)
+
   cy.get('button').click()
 
   cy.get('input').should('have.length', 2)
