bf2cf76702f1ad47471b02e9ad53f37f9c165b7c
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-30 13:14:23 +0300

    test(<NotesList>): add unit-tests

---
+++ src/components/tests/NotesList.spec.tsx
@@ -0,0 +1,62 @@
+import { mount } from '@cypress/react'
+import { RelayEnvironmentProvider } from 'react-relay'
+import RelayEnvironment from '../../utils/RelayEnvironment'
+import NotesListWrapper from '../NotesList'
+import { NotesListQueryResponse } from '../__generated__/NotesListQuery.graphql'
+import { SERVER_URL } from './constants'
+
+const mockData: NotesListQueryResponse = {
+  notes: [
+    {
+      id: 'Test ID',
+      title: 'Test Title 1',
+      description: 'Test Description 1',
+    },
+    {
+      id: 'Test ID 2',
+      title: 'Test Title 2',
+      description: 'Test Description 2',
+    },
+  ],
+}
+
+const setUpComponent = (haveToken = false) => {
+  if (haveToken) localStorage.setItem('token', 'testtoken')
+
+  cy.intercept(SERVER_URL, {
+    data: mockData,
+  }).as('fetch')
+  mount(
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <NotesListWrapper />
+    </RelayEnvironmentProvider>
+  )
+}
+
+it('Display message "Please, log in" if there isnt token in localstorage', () => {
+  setUpComponent()
+  cy.get('h1').should('have.text', 'Please, log in')
+})
+
+it('Render correctly if there is token in localstorage', () => {
+  setUpComponent(true)
+  cy.get('section')
+    .first()
+    .should('include.text', mockData.notes[0].title)
+    .and('include.text', mockData.notes[0].description)
+    .and('include.html', 'button')
+
+  cy.get('section')
+    .last()
+    .should('include.text', mockData.notes[1].title)
+    .and('include.text', mockData.notes[1].description)
+    .and('include.html', 'button')
+
+  cy.get('button').contains('Add Note').click()
+
+  cy.get('button').contains('Save')
+  cy.get('input').should('have.length', 2)
+
+  cy.get('button').contains('Cancel').click()
+  cy.get('#__cy_root').should('not.include.html', 'input')
+})
